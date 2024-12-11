546cfddad2b7e947317b3cfd86034d1084c9f55f
root <root@Drew.>
2024-12-11 17:52:09 -0500

    rewrote some anlysis and added terms so selenium works on linux

--- analysis.py
+++ analysis.py
@@ -3,27 +3,22 @@ from config import config
 
 class Analysis:
     def __init__(self):
-        self.offensive_data = self.get_offensive_stats()
-        self.defensive_data = self.get_defensive_stats()
-        self.st_data = self.get_special_teams_stats()
-    
-    def get_offensive_stats(self):
-        offensive_stats = config['offense_stat_list']
+        self.offensive_data = self.create_team_stats_dfs('offense')
+        self.defensive_data = self.create_team_stats_dfs('defense')
+        self.st_data = self.create_team_stats_dfs('special_teams')
+
+    def create_team_stats_dfs(self, side):
+        stats = config[f'{side}_stat_list']
         data = {}
-        for stat in offensive_stats:
-            data[stat] = pd.read_csv(f'{config['team_outputs']}/offense/offense_{stat}_output.csv')
-           
+        for stat in stats:
+            data[stat] = pd.read_csv(f'{config['team_outputs']}/{side}/{side}_{stat}_output.csv')
+
         return data
     
-    def get_defensive_stats(self):
-        defensive_stats = config['defense_stat_list']
-        data = {}
-        for stat in defensive_stats:
-            data[stat] = pd.read_csv(f'{config['team_outputs']}/defense/defense_{stat}_output.csv')
-        return data
-    def get_special_teams_stats(self):
-        st_stats = config['special_teams_stat_list']
-        data = {}
-        for stat in st_stats:
-            data[stat] = pd.read_csv(f'{config['team_outputs']}/special_teams/special_teams_{stat}_output.csv')
-        return data
+    def get_max_stat(self, df, stat):
+        print(df[stat].idxmax())
+        return df.loc[df[stat].idxmax()]
+    
+    def team_order_stat(self, df, stat):
+        return df.sort_values(by=[stat])
+    
